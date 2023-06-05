7f33ca1c6dd70759c9041be2f8aba16ad83de985
Kapuetze <joni@vocalpatrioten.de>
2023-06-05 04:29:22 +0200

    GameLoop

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -5,32 +5,65 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public static GameController INSTANCE;
+    public static GameController instance;
 
     public string playerPrefabName;
 
+    public Vector3 cardSpawnPosition;
+
+    public GameState gameState;
+
     [SerializeField]
     private Card[] cardTypes;
+    [SerializeField]
+    private int timeBetweenCards = 3;
+    [SerializeField]
+    private int timeAfterRoundEnd = 5;
 
     /// <summary>
-    /// The card that is currently in looked for
+    /// The card that is currently wanted
     /// </summary>
     private int wantedCardIndex = 0;
 
     /// <summary>
     /// Card that is currently in play
     /// </summary>
-    private Card currentCardInPlay;
+    private GameObject currentCardInPlay;
+
+    private float timer = 0;
 
     private void Start()
     {
-        if(INSTANCE)
+        if(instance)
         {
             Destroy(this);
         }
         else
         {
-            INSTANCE = this;
+            instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        switch (gameState)
+        {
+            case GameState.CyclingCards:
+                if (timer >= timeBetweenCards)
+                {
+                    ServerSpawnNextCard();
+                    timer = 0;
+                }
+                break;
+            case GameState.WaitingForNextRound:
+                if (timer >= timeAfterRoundEnd)
+                {
+                    RestartRound();
+                    timer = 0;
+                }
+                break;
         }
     }
 
@@ -48,12 +81,55 @@ public class GameController : MonoBehaviour
         PhotonNetwork.Instantiate(playerPrefabName, new Vector2(0,0), Quaternion.identity);
     }
 
+    public void HandlePlayerInteraction()
+    {
+        // TODO: Break the game loop when user tapped the card
+        // Either they were correct or punish them if they are wrong
+    }
+
     /// <summary>
     /// Grabs a random card and displays it for the players to tap on
     /// Card probability is weighted by the amount in the card scriptable object
     /// </summary>
-    public void SpawnNextCard()
+    public void ServerSpawnNextCard()
     {
-        PhotonNetwork.Instantiate("", new Vector2(0, 0), Quaternion.identity);
+        // Get Random card
+        int randomIndex = Random.Range(0, cardTypes.Length);
+        RpcSpawnCard(randomIndex);
+        //PhotonNetwork.Instantiate("", new Vector2(0, 0), Quaternion.identity);
     }
+
+    public void RpcSpawnCard(int cardIndex)
+    {
+        Destroy(currentCardInPlay);
+        currentCardInPlay = Instantiate(cardTypes[cardIndex].gameObject, cardSpawnPosition, Quaternion.identity);
+
+        CycleWantedCard();
+    }
+
+    private void RestartRound()
+    {
+        ServerSpawnNextCard();
+    }
+
+    /// <summary>
+    /// Increase the wanted card index by one and reset it if it exceeds the array bounds
+    /// Then display the wanted card in UI
+    /// </summary>
+    private void CycleWantedCard()
+    {
+        wantedCardIndex++;
+        if (wantedCardIndex >= cardTypes.Length)
+        {
+            wantedCardIndex = 0;
+        }
+
+        // TODO: Display wanted card in UI
+    }
+}
+
+public enum GameState
+{
+    CyclingCards,
+    WaitingForNextRound,
 }
